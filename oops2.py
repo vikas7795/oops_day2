93a39f4ddce16bf123511bbc7ec309aeb82961e6
vikas <4MU22CS094@MYSURUROYAL.ORG>
2026-02-04 10:25:14 +0530

    initial commit

---
+++ oops2.py
@@ -0,0 +1,28 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    def display_book_details(self):
+        print("Book Title:", self.title)
+        print("Author:", self.author)
+
+class IssuedBook(Book):
+    def __init__(self, title, author, issued_to, issued_date):
+        super().__init__(title, author)
+        self.issued_to = issued_to
+        self.issued_date = issued_date
+
+    def display_issued_book_details(self):
+        self.display_book_details()
+        print("Issued To:", self.issued_to)
+        print("Issued Date:", self.issued_date)
+
+issued_book1 = IssuedBook(
+    "Python Programming",
+    "Guido van Rossum",
+    "Ramya",
+    "2026-02-04"
+)
+
+issued_book1.display_issued_book_details()
