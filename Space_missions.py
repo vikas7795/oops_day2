f736d1bacdbefc0edf2f4ad58886baab3a4df32a
vikas7795 <4mu22cs094@mysururoyal.org>
2026-02-09 22:17:31 +0530

    Add files via upload

---
+++ Space_missions.py
@@ -0,0 +1,13 @@
+#Space_missions
+class Robot:
+    def communicate(self):
+        return "Beep beep."
+
+class ExplorerRobot(Robot):
+    def communicate(self):
+        return "Exploring new planets!"
+
+r = Robot()
+e = ExplorerRobot()
+print(r.communicate())
+print(e.communicate())
