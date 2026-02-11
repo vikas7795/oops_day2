225843cd361ce6a1887bfacc78827035d99119ef
vikas7795 <4mu22cs094@mysururoyal.org>
2026-02-11 23:34:45 +0530

    Add files via upload

---
+++ Dayy10/Counter_Custom_Iterator.py
@@ -0,0 +1,20 @@
+class Counter:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+        self.current = start
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current <= self.end:
+            value = self.current
+            self.current += 1
+            return value
+        else:
+            raise StopIteration
+
+
+for i in Counter(1, 5):
+    print(i)
